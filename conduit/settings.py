65b9553f2c666f837510c735360af0cb2c969684
ibrahim <thkbprbxyg@privaterelay.appleid.com>
2026-02-23 09:58:01 +0100

    Fix CORS_ALLOWED_ORIGINS assignment in settings.py

--- conduit/settings.py
+++ conduit/settings.py
@@ -133,7 +133,8 @@ STATIC_ROOT = os.path.join (BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CORS_ALLOWED_ORIGINS = os.environ.get(
-    "CORS_ALLOWED_ORIGINS" = 'http://<your_ip>:8282/'
+    "CORS_ALLOWED_ORIGINS" 
+    "http://<your_ip>:8282/"
 ).split(",")
 
 # Tell Django about the custom `User` model we created. The string
